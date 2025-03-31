0c41722bd82fde22a0716c758b5a1312d99fa5f1
xiadong <xiadaoliang@outlook.com>
2025-03-31 17:31:05 +0800

    fix filelist bug

--- synthesis/filelist.f
+++ synthesis/filelist.f
@@ -7,7 +7,7 @@ $MCASH_HOME/synthesis/src/commons/rob_id_gen.v
 
 $MCASH_HOME/synthesis/src/commons/mem/f_spsram_large.v
 $MCASH_HOME/synthesis/src/commons/mem/ram.v
-$MCASH_HOME/synthesis/src/commons/axi_slave.v
+$MCASH_HOME/synthesis/src/commons/axi_slave_ram.v
 
 $MCASH_HOME/synthesis/src/mcash_top.v
 
