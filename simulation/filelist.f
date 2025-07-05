3f05970e8fd2f38d0ca6681c89acfb02ab767824
xiadong <xiadaoliang@outlook.com>
2025-07-05 08:27:39 -0700

    update filelist

---
+++ simulation/filelist.f
@@ -0,0 +1,15 @@
++incdir+$MCASH_HOME/simulation/src/assert/include
+$MCASH_HOME/simulation/src/assert/cross_bar_assert.sv
+$MCASH_HOME/simulation/src/assert/cross_bar_rob_assert.sv
+$MCASH_HOME/simulation/src/assert/spw_buffer_assert.sv
+$MCASH_HOME/simulation/src/assert/assert_binder.sv
+
+$MCASH_HOME/simulation/src/dumper/mcash_dumper.sv
+$MCASH_HOME/simulation/src/dumper/mcash_diff.sv
+
+$MCASH_HOME/synthesis/src/commons/mem/f_spsram_large.v
+$MCASH_HOME/synthesis/src/commons/mem/ram.v
+$MCASH_HOME/synthesis/src/commons/axi_slave_ram.v
+$MCASH_HOME/synthesis/src/commons/amba_axi_m4s4.v
+$MCASH_HOME/simulation/src/tb/mcash_tb.sv
+$MCASH_HOME/golden/build/golden.so
--- synthesis/filelist.f
+++ synthesis/filelist.f
@@ -1,12 +1,3 @@
-+incdir+$MCASH_HOME/simulation/src/assert/include
-$MCASH_HOME/simulation/src/dumper/mcash_dumper.sv
-$MCASH_HOME/simulation/src/dumper/mcash_diff.sv
-$MCASH_HOME/simulation/src/assert/cross_bar_assert.sv
-$MCASH_HOME/simulation/src/assert/cross_bar_rob_assert.sv
-$MCASH_HOME/simulation/src/assert/spw_buffer_assert.sv
-$MCASH_HOME/simulation/src/assert/assert_binder.sv
-$MCASH_HOME/simulation/src/tb/mcash_tb.sv
-$MCASH_HOME/golden/build/golden.so
 $MCASH_HOME/synthesis/src/commons/sync_fifo.v
 $MCASH_HOME/synthesis/src/commons/biu_fifo_2i1o.v
 $MCASH_HOME/simulation/src/commons/ram_sp.v
@@ -14,10 +5,16 @@ $MCASH_HOME/synthesis/src/commons/round_robin_arbiter.v
 $MCASH_HOME/synthesis/src/commons/rob_id_gen.v
 
 $MCASH_HOME/synthesis/include/mcash_config.v
-$MCASH_HOME/synthesis/src/commons/mem/f_spsram_large.v
-$MCASH_HOME/synthesis/src/commons/mem/ram.v
-$MCASH_HOME/synthesis/src/commons/axi_slave_ram.v
+
 $MCASH_HOME/synthesis/src/commons/wbuffer_freelist.v
+
+$MCASH_HOME/synthesis/src/cross_bar/channel_entry_rr.v
+$MCASH_HOME/synthesis/src/cross_bar/bank_ch_rr.v
+$MCASH_HOME/synthesis/src/cross_bar/cross_bar_core.v
+$MCASH_HOME/synthesis/src/cross_bar/cross_bar_core_buffer.v
+$MCASH_HOME/synthesis/src/cross_bar/cross_bar_rob.v
+$MCASH_HOME/synthesis/src/cross_bar/spw_buffer.v
+$MCASH_HOME/synthesis/src/cross_bar/cross_bar_top.v
 
 $MCASH_HOME/synthesis/src/mcash_top.v
 $MCASH_HOME/synthesis/src/bank/bank_top_wrapper.v
@@ -36,16 +33,6 @@ $MCASH_HOME/synthesis/src/bank/bank_sram_controller.v
 $MCASH_HOME/synthesis/src/bank/bank_wbuffer.v
 $MCASH_HOME/synthesis/src/bank/bank_biu_top.v
 
-$MCASH_HOME/synthesis/src/cross_bar/channel_entry_rr.v
-$MCASH_HOME/synthesis/src/cross_bar/bank_ch_rr.v
-$MCASH_HOME/synthesis/src/cross_bar/cross_bar_core.v
-$MCASH_HOME/synthesis/src/cross_bar/cross_bar_core_buffer.v
-$MCASH_HOME/synthesis/src/cross_bar/cross_bar_rob.v
-$MCASH_HOME/synthesis/src/cross_bar/spw_buffer.v
-$MCASH_HOME/synthesis/src/cross_bar/cross_bar_top.v
-
-
-$MCASH_HOME/synthesis/src/commons/amba_axi_m4s4.v
 $MCASH_HOME/synthesis/src/commons/priority_aribiter.v
 $MCASH_HOME/synthesis/src/commons/shift_priority_arb.v
 $MCASH_HOME/synthesis/src/commons/shift_priority_arb_256.v
